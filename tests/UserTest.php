06d27504256c6eb46b80b02b433863ebf267768e
LemouD <lemoundiop@gmail.com>
2020-04-09 11:53:26 +0200

    exo3 tearDown methods

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -9,15 +9,37 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    /** @var User $user */
+    private $user;
+
+    public function setUp(): void
+    {
+        $this->user = new User('Ryanlab@email.com', 'Ryan', 'Lab', 21);
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->user);
+    }
+
     public function testIsValid()
     {
-        $user = new User('ryanlab@email.com', 'Ryan', 'Lab', 22);
-        $this->assertTrue($user->isValid());
+        $this->assertTrue($this->user->isValid());
 
-        $user = new User('ryanlabemail', 'Ryan', 'Lab', 22);
-        $this->assertFalse($user->isValid());
-        
-        $user = new User('ryanlab@email.com', 'Ryan', 'Lab', 11);
-        $this->assertFalse($user->isValid());
+        $this->user->setEmail('Ryanlabemail');
+        $this->assertFalse($this->user->isValid());
+        $this->user->setEmail('Ryanlab@email.com');
+
+        $this->user->setFirstname('');
+        $this->assertFalse($this->user->isValid());
+        $this->user->setFirstname('Ryan');
+
+        $this->user->setLastname('');
+        $this->assertFalse($this->user->isValid());
+        $this->user->setLastname('Lab');
+
+        $this->user->setAge(15);
+        $this->assertFalse($this->user->isValid());
+        $this->user->setAge(24);
     }
 }
