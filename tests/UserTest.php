906ba78ee957462cc404467f791544216d5060eb
LemouD <lemoundiop@gmail.com>
2020-04-09 11:18:49 +0200

    exo3-s

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -11,19 +11,13 @@ class UserTest extends TestCase
 {
     public function testIsValid()
     {
-        $user = new User('good@email.com', 'Ryan', 'Lab', 22);
+        $user = new User('ryanlab@email.com', 'Ryan', 'Lab', 22);
         $this->assertTrue($user->isValid());
 
-        $user = new User('bademail', 'Ryan', 'Lab', 22);
+        $user = new User('ryanlabemail', 'Ryan', 'Lab', 22);
         $this->assertFalse($user->isValid());
-
-        $user = new User('good@email.com', '', 'Lab', 22);
-        $this->assertFalse($user->isValid());
-
-        $user = new User('good@email.com', 'Ryan', '', 22);
-        $this->assertFalse($user->isValid());
-
-        $user = new User('good@email.com', 'Ryan', 'Lab', 13);
+        
+        $user = new User('ryanlab@email.com', 'Ryan', 'Lab', 11);
         $this->assertFalse($user->isValid());
     }
 }
